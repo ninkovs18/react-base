7fdfd6e616e1289f1a685f3a37a5d887f5be2a1b
Strahinja1520 <strahinja1520@its.edu.rs>
2024-06-01 15:46:59 +0200

    css loader & allow images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./style.css";
+import imageJPG from "./react-logo.jpg";
+import imageSVG from "./react-1-logo.svg";
 
 export const App = () => {
-  return <h1>REACT BASE</h1>;
+  return (
+    <div>
+      <h1>REACT BASE</h1>
+      <img src={imageJPG} alt="React logo" width={300} height={300} />
+      <img src={imageSVG} alt="React logo" width={300} height={300} />
+    </div>
+  );
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -12,11 +12,19 @@ module.exports = {
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: "asset/inline",
       },
     ],
   },
